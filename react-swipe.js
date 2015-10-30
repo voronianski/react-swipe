55a258337f7e80fa90a55bb99dd032427d4b01bf
Alton Torregano <alton.torregano@c4tech.com>
2015-10-30 10:59:31 -0500

    formatting

--- react-swipe.js
+++ react-swipe.js
@@ -1,94 +1,94 @@
 (function (root, factory) {
-  if (typeof define === 'function' && define.amd) {
-    // AMD
-    define(['react', 'react-dom', 'swipe-js-iso'], factory);
-  }else if (typeof exports === 'object') {
-    module.exports = factory(
-        require('react'),
-        require('react-dom'),
-        require('swipe-js-iso')
-    );
-  } else {
-    root.ReactSwipe = factory(
-        root.React,
-        root.ReactDOM,
-      root.Swipe
-    );
-  }
-})(this, function (React, ReactDOM, Swipe ) {
-  var styles = {
-    container: {
-      overflow: 'hidden',
-      visibility: 'hidden',
-      position: 'relative'
-    },
-
-    wrapper: {
-      overflow: 'hidden',
-      position: 'relative'
-    },
-
-    child: {
-      float: 'left',
-      width: '100%',
-      position: 'relative',
-      transitionProperty: 'transform'
+    if (typeof define === 'function' && define.amd) {
+        // AMD
+        define(['react', 'react-dom', 'swipe-js-iso'], factory);
+    } else if (typeof exports === 'object') {
+        module.exports = factory(
+            require('react'),
+            require('react-dom'),
+            require('swipe-js-iso')
+        );
+    } else {
+        root.ReactSwipe = factory(
+            root.React,
+            root.ReactDOM,
+            root.Swipe
+        );
     }
-  };
+})(this, function (React, ReactDOM, Swipe) {
+    var styles = {
+        container: {
+            overflow: 'hidden',
+            visibility: 'hidden',
+            position: 'relative'
+        },
 
-  var ReactSwipe = React.createClass({
-    // https://github.com/thebird/Swipe#config-options
-    propTypes: {
-      startSlide      : React.PropTypes.number,
-      slideToIndex    : React.PropTypes.number,
-      shouldUpdate    : React.PropTypes.func,
-      speed           : React.PropTypes.number,
-      auto            : React.PropTypes.number,
-      continuous      : React.PropTypes.bool,
-      disableScroll   : React.PropTypes.bool,
-      stopPropagation : React.PropTypes.bool,
-      callback        : React.PropTypes.func,
-      transitionEnd   : React.PropTypes.func
-    },
+        wrapper: {
+            overflow: 'hidden',
+            position: 'relative'
+        },
 
-    componentDidMount: function () {
-      if (this.isMounted()) {
-        this.swipe = Swipe(ReactDOM.findDOMNode(this), Object.assign({}, this.props));
-      }
-    },
+        child: {
+            float: 'left',
+            width: '100%',
+            position: 'relative',
+            transitionProperty: 'transform'
+        }
+    };
 
-    componentDidUpdate: function () {
-      if (this.props.slideToIndex || this.props.slideToIndex === 0) {
-        this.swipe.slide(this.props.slideToIndex);
-      }
-    },
+    var ReactSwipe = React.createClass({
+        // https://github.com/thebird/Swipe#config-options
+        propTypes: {
+            startSlide: React.PropTypes.number,
+            slideToIndex: React.PropTypes.number,
+            shouldUpdate: React.PropTypes.func,
+            speed: React.PropTypes.number,
+            auto: React.PropTypes.number,
+            continuous: React.PropTypes.bool,
+            disableScroll: React.PropTypes.bool,
+            stopPropagation: React.PropTypes.bool,
+            callback: React.PropTypes.func,
+            transitionEnd: React.PropTypes.func
+        },
 
-    componentWillUnmount: function () {
-      this.swipe.kill();
-      delete this.swipe;
-    },
+        componentDidMount: function () {
+            if (this.isMounted()) {
+                this.swipe = Swipe(ReactDOM.findDOMNode(this), Object.assign({}, this.props));
+            }
+        },
 
-    shouldComponentUpdate: function (nextProps) {
-      return (
-        (this.props.slideToIndex !== nextProps.slideToIndex) ||
-        (typeof this.props.shouldUpdate !== 'undefined') && this.props.shouldUpdate(nextProps)
-      );
-    },
+        componentDidUpdate: function () {
+            if (this.props.slideToIndex || this.props.slideToIndex === 0) {
+                this.swipe.slide(this.props.slideToIndex);
+            }
+        },
 
-    render: function() {
-      return React.createElement('div', React.__spread({}, {style: styles.container}, this.props),
-        React.createElement('div', {style: styles.wrapper},
-          React.Children.map(this.props.children, function (child) {
-            return React.cloneElement(child, {
-              ref: child.props.ref,
-              key: child.props.key,
-              style: child.props.style ? Object.assign(child.props.style, styles.child) : styles.child
-            });
-          })
-        )
-      );
-    }
-  });
+        componentWillUnmount: function () {
+            this.swipe.kill();
+            delete this.swipe;
+        },
 
-  return ReactSwipe;
+        shouldComponentUpdate: function (nextProps) {
+            return (
+                (this.props.slideToIndex !== nextProps.slideToIndex) ||
+                (typeof this.props.shouldUpdate !== 'undefined') && this.props.shouldUpdate(nextProps)
+            );
+        },
+
+        render: function () {
+            return React.createElement('div', React.__spread({}, {style: styles.container}, this.props),
+                React.createElement('div', {style: styles.wrapper},
+                    React.Children.map(this.props.children, function (child) {
+                        return React.cloneElement(child, {
+                            ref: child.props.ref,
+                            key: child.props.key,
+                            style: child.props.style ? Object.assign(child.props.style, styles.child) : styles.child
+                        });
+                    })
+                )
+            );
+        }
+    });
+
+    return ReactSwipe;
 });
