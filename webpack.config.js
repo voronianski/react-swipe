23d86ece99f23bd0b3aa37a938c673cb0acdfaed
Dmitri Voronianski <dmitri.voronianski@gmail.com>
2016-04-02 22:07:59 +0300

    add dist

---
+++ webpack.config.js
@@ -0,0 +1,60 @@
+'use strict';
+
+const env = process.env.NODE_ENV || 'development';
+
+const webpack = require('webpack');
+const path = require('path');
+const webpackUMDExternal = require('webpack-umd-external');
+
+const pluginsList = [];
+const outputFileName = env === 'production' ?
+    'react-swipe.min.js' :
+    'react-swipe.js';
+
+if (env === 'production') {
+    pluginsList.push(
+        new webpack.optimize.UglifyJsPlugin({
+            compress: { warnings: false },
+            output: { comments: false }
+        })
+    );
+}
+
+const config = {
+    entry: path.join(__dirname, 'src/ReactSwipe.js'),
+
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: outputFileName,
+        library: 'ReactSwipe',
+        libraryTarget: 'umd',
+        umdNamedDefine: true
+    },
+
+    externals: webpackUMDExternal({
+        'react': 'React',
+        'swipe-js-iso': 'Swipe'
+    }),
+
+    resolve: {
+        extensions: ['', '.js', '.jsx']
+    },
+
+    plugins: pluginsList,
+
+    module: {
+        preLoaders: [{
+            test: /\.jsx?$/,
+            loaders: ['eslint'],
+            exclude: /node_modules/
+        }],
+
+        loaders: [{
+            test: /\.jsx?$/,
+            exclude: /node_modules/,
+            loaders: ['babel']
+        }]
+    }
+};
+
+module.exports = config;
