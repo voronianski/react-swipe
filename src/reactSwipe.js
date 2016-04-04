cdea5543d6b3d8ab148d9d608e167b5269e973a5
Alexander Sundli-Härdig <alexander.sundli-hardig@keyteq.no>
2016-04-04 16:02:51 +0200

    Avoid undefined className
    To avoid 'react-swipe-container undefined' className when not sending className as props

--- src/reactSwipe.js
+++ src/reactSwipe.js
@@ -43,7 +43,8 @@ class ReactSwipe extends Component {
                 position: 'relative',
                 transitionProperty: 'transform'
             }
-        }
+        },
+        className: ''
     };
 
     componentDidMount() {
