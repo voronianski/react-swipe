30ec0fe92c0f1938350e0d71f0f00d56bc3c64cd
Dmitri Voronianski <dmitri.voronianski@gmail.com>
2016-11-20 22:28:22 +0100

    adjust new version

--- demo/index.js
+++ demo/index.js
@@ -8,59 +8,59 @@ const query = querystring.parse(window.location.search.slice(1));
 // generate slide panes
 const numberOfSlides = parseInt(query.slidesNum, 10) || 20;
 const paneNodes = Array.apply(null, Array(numberOfSlides)).map((_, i) => {
-    return (
-        <div key={i}>
-            <div className="item">{i}</div>
-        </div>
-    );
+  return (
+    <div key={i}>
+      <div className="item">{i}</div>
+    </div>
+  );
 });
 
 // change Swipe.js options by query params
 const startSlide = parseInt(query.startSlide, 10) || 0;
 const swipeOptions = {
-    startSlide: startSlide < paneNodes.length && startSlide >= 0 ? startSlide : 0,
-    auto: parseInt(query.auto, 10) || 0,
-    speed: parseInt(query.speed, 10) || 300,
-    disableScroll: query.disableScroll === 'true',
-    continuous: query.continuous === 'true',
-    callback() {
-        console.log('slide changed');
-    },
-    transitionEnd() {
-        console.log('ended transition');
-    }
+  startSlide: startSlide < paneNodes.length && startSlide >= 0 ? startSlide : 0,
+  auto: parseInt(query.auto, 10) || 0,
+  speed: parseInt(query.speed, 10) || 300,
+  disableScroll: query.disableScroll === 'true',
+  continuous: query.continuous === 'true',
+  callback() {
+    console.log('slide changed');
+  },
+  transitionEnd() {
+    console.log('ended transition');
+  }
 };
 
 class Page extends Component {
-    next() {
-        this.refs.reactSwipe.next();
-    }
+  next() {
+    this.refs.reactSwipe.next();
+  }
 
-    prev() {
-        this.refs.reactSwipe.prev();
-    }
+  prev() {
+    this.refs.reactSwipe.prev();
+  }
 
-    render() {
-        return (
-            <div className="center">
-                <h1>ReactSwipe.js</h1>
-                <h2>Open this page from a mobile device (real or emulated).</h2>
-                <h2>You can pass <a href="https://github.com/voronianski/swipe-js-iso#config-options">Swipe.js options</a> as query params.</h2>
+  render() {
+    return (
+      <div className="center">
+        <h1>ReactSwipe.js</h1>
+        <h2>Open this page from a mobile device (real or emulated).</h2>
+        <h2>You can pass <a href="https://github.com/voronianski/swipe-js-iso#config-options">Swipe.js options</a> as query params.</h2>
 
-                <ReactSwipe ref="reactSwipe" className="mySwipe" swipeOptions={swipeOptions}>
-                    {paneNodes}
-                </ReactSwipe>
+        <ReactSwipe ref="reactSwipe" className="mySwipe" swipeOptions={swipeOptions}>
+            {paneNodes}
+        </ReactSwipe>
 
-                <div>
-                    <button type="button" onClick={::this.prev}>Prev</button>
-                    <button type="button" onClick={::this.next}>Next</button>
-                </div>
-            </div>
-        );
-    }
+        <div>
+          <button type="button" onClick={::this.prev}>Prev</button>
+          <button type="button" onClick={::this.next}>Next</button>
+        </div>
+      </div>
+    );
+  }
 }
 
 ReactDOM.render(
-    <Page />,
-    document.getElementById('app')
+  <Page />,
+  document.getElementById('app')
 );
--- dist/react-swipe.js
+++ dist/react-swipe.js
@@ -70,6 +70,10 @@ return /******/ (function(modules) { // webpackBootstrap
 
 	var _swipeJsIso2 = _interopRequireDefault(_swipeJsIso);
 
+	var _objectAssign = __webpack_require__(3);
+
+	var _objectAssign2 = _interopRequireDefault(_objectAssign);
+
 	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
@@ -145,7 +149,9 @@ return /******/ (function(modules) { // webpackBootstrap
 	          'div',
 	          { style: style.wrapper },
 	          _react2.default.Children.map(children, function (child) {
-	            return _react2.default.cloneElement(child, { style: style.child });
+	            return _react2.default.cloneElement(child, {
+	              style: child.props.style ? (0, _objectAssign2.default)(child.props.style, style.child) : style.child
+	            });
 	          })
 	        )
 	      );
@@ -212,6 +218,95 @@ return /******/ (function(modules) { // webpackBootstrap
 /***/ function(module, exports) {
 
 	module.exports = __WEBPACK_EXTERNAL_MODULE_2__;
+
+/***/ },
+/* 3 */
+/***/ function(module, exports) {
+
+	'use strict';
+	/* eslint-disable no-unused-vars */
+	var hasOwnProperty = Object.prototype.hasOwnProperty;
+	var propIsEnumerable = Object.prototype.propertyIsEnumerable;
+
+	function toObject(val) {
+		if (val === null || val === undefined) {
+			throw new TypeError('Object.assign cannot be called with null or undefined');
+		}
+
+		return Object(val);
+	}
+
+	function shouldUseNative() {
+		try {
+			if (!Object.assign) {
+				return false;
+			}
+
+			// Detect buggy property enumeration order in older V8 versions.
+
+			// https://bugs.chromium.org/p/v8/issues/detail?id=4118
+			var test1 = new String('abc');  // eslint-disable-line
+			test1[5] = 'de';
+			if (Object.getOwnPropertyNames(test1)[0] === '5') {
+				return false;
+			}
+
+			// https://bugs.chromium.org/p/v8/issues/detail?id=3056
+			var test2 = {};
+			for (var i = 0; i < 10; i++) {
+				test2['_' + String.fromCharCode(i)] = i;
+			}
+			var order2 = Object.getOwnPropertyNames(test2).map(function (n) {
+				return test2[n];
+			});
+			if (order2.join('') !== '0123456789') {
+				return false;
+			}
+
+			// https://bugs.chromium.org/p/v8/issues/detail?id=3056
+			var test3 = {};
+			'abcdefghijklmnopqrst'.split('').forEach(function (letter) {
+				test3[letter] = letter;
+			});
+			if (Object.keys(Object.assign({}, test3)).join('') !==
+					'abcdefghijklmnopqrst') {
+				return false;
+			}
+
+			return true;
+		} catch (e) {
+			// We don't expect any of the above to throw, but better to be safe.
+			return false;
+		}
+	}
+
+	module.exports = shouldUseNative() ? Object.assign : function (target, source) {
+		var from;
+		var to = toObject(target);
+		var symbols;
+
+		for (var s = 1; s < arguments.length; s++) {
+			from = Object(arguments[s]);
+
+			for (var key in from) {
+				if (hasOwnProperty.call(from, key)) {
+					to[key] = from[key];
+				}
+			}
+
+			if (Object.getOwnPropertySymbols) {
+				symbols = Object.getOwnPropertySymbols(from);
+				for (var i = 0; i < symbols.length; i++) {
+					if (propIsEnumerable.call(from, symbols[i])) {
+						to[symbols[i]] = from[symbols[i]];
+					}
+				}
+			}
+		}
+
+		return to;
+	};
+
 
 /***/ }
 /******/ ])
