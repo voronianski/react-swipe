9ec766dfdd9bb858c89713b88ecf3c8fcab8bf04
Dmitri Voronianski <dmitri.voronianski@gmail.com>
2017-04-21 15:46:14 +0200

    version update and build

--- webpack.config.js
+++ webpack.config.js
@@ -8,53 +8,53 @@ const webpackUMDExternal = require('webpack-umd-external');
 
 const pluginsList = [];
 const outputFileName = env === 'production' ?
-    'react-swipe.min.js' :
-    'react-swipe.js';
+  'react-swipe.min.js' :
+  'react-swipe.js';
 
 if (env === 'production') {
-    pluginsList.push(
-        new webpack.optimize.UglifyJsPlugin({
-            compress: { warnings: false },
-            output: { comments: false }
-        })
-    );
+  pluginsList.push(
+    new webpack.optimize.UglifyJsPlugin({
+      compress: { warnings: false },
+      output: { comments: false }
+    })
+  );
 }
 
 const config = {
-    entry: path.join(__dirname, 'src/reactSwipe.js'),
+  entry: path.join(__dirname, 'src/reactSwipe.js'),
 
-    output: {
-        path: path.join(__dirname, 'dist'),
-        filename: outputFileName,
-        library: 'ReactSwipe',
-        libraryTarget: 'umd',
-        umdNamedDefine: true
-    },
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: outputFileName,
+    library: 'ReactSwipe',
+    libraryTarget: 'umd',
+    umdNamedDefine: true
+  },
 
-    externals: webpackUMDExternal({
-        'react': 'React',
-        'swipe-js-iso': 'Swipe'
-    }),
+  externals: webpackUMDExternal({
+    'react': 'React',
+    'swipe-js-iso': 'Swipe'
+  }),
 
-    resolve: {
-        extensions: ['', '.js', '.jsx']
-    },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
 
-    plugins: pluginsList,
+  plugins: pluginsList,
 
-    module: {
-        preLoaders: [{
-            test: /\.jsx?$/,
-            loaders: ['eslint'],
-            exclude: /node_modules/
-        }],
+  module: {
+    preLoaders: [{
+      test: /\.jsx?$/,
+      loaders: ['eslint'],
+      exclude: /node_modules/
+    }],
 
-        loaders: [{
-            test: /\.jsx?$/,
-            exclude: /node_modules/,
-            loaders: ['babel']
-        }]
-    }
+    loaders: [{
+      test: /\.jsx?$/,
+      exclude: /node_modules/,
+      loaders: ['babel']
+    }]
+  }
 };
 
 module.exports = config;
