fa1f9a05ec0e720fa6521d294752c2173bf624f1
Christophe Torre <christophe.torre@hotmail.fr>
2020-04-06 15:02:55 +0200

    replace lodash by custom deepEqual to reduce bundle size

---
+++ src/deepEqual.js
@@ -0,0 +1,17 @@
+export default function deepEqual(a, b) {
+  if (a === b) return true;
+
+  if (a == null || typeof a != 'object' || b == null || typeof b != 'object')
+    return false;
+
+  let keysA = Object.keys(a),
+    keysB = Object.keys(b);
+
+  if (keysA.length != keysB.length) return false;
+
+  for (let key of keysA) {
+    if (!keysB.includes(key) || !deepEqual(a[key], b[key])) return false;
+  }
+
+  return true;
+}
