6359eb65f0c222e35b129f64773cf2306764dbd2
Christophe Torre <christophe.torre@hotmail.fr>
2020-04-09 11:10:16 +0200

    fix auto review issues

--- src/deepEqual.js
+++ src/deepEqual.js
@@ -1,13 +1,23 @@
 export default function deepEqual(a, b) {
-  if (a === b) return true;
+  if (a === b) {
+    return true;
+  }
 
-  if (a == null || typeof a != 'object' || b == null || typeof b != 'object')
+  if (
+    a === null ||
+    typeof a !== 'object' ||
+    b === null ||
+    typeof b !== 'object'
+  ) {
     return false;
+  }
 
   let keysA = Object.keys(a),
     keysB = Object.keys(b);
 
-  if (keysA.length != keysB.length) return false;
+  if (keysA.length !== keysB.length) {
+    return false;
+  }
 
   for (let key of keysA) {
     if (!keysB.includes(key) || !deepEqual(a[key], b[key])) return false;
